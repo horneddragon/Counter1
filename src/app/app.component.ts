d6329f1fb76dcf1474d1625b3916c64cfeb052c5
V ANIRUDHA PURANIC <anirudhapuranic@outlook.com>
2021-01-24 23:38:25 +0530

    mt initial commet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter1';
+  title: string = 'counter1';
+  myName: string = 'Ani';
+  City: string = 'Belagavi';
+  gpa: number = 9;
+  isindiancitizen: boolean = true;
+
+
+  myUserObject: any = {
+    "title": 'counter1',
+    "myName": 'Ani',
+    "gpa": 9,
+    "isindiancitizen": true,
+    "myAddressesObject": {
+
+      "myHomeAddressObject": {
+        "City": 'Belagavi',
+        "pincode": 590002
+      },
+
+      "myOfficeAddressObject": {
+        "City": 'Ballari',
+        "pincode": 583101
+      }
+    }
+
+  };
+
+
+
+  myLevels = [1, 2, 3, 4, 5];
+
+  constructor() {
+
+  }
+
+  test() {
+    let obj = {
+      "title": 'counter1',
+      "myName": 'Ani',
+      "City": 'Belagavi',
+      "gpa": 9,
+      "isindiancitizen": true,
+    };
+  }
 }
